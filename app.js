a7f9e9cf3eafa160b76a01eba67bc2237bb3f449
rrizkie <rizkieawanj@gmail.com>
2020-08-27 18:52:40 +0700

    add session

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const routes = require('./routes');
+const session = require('express-session')
+
+app.use(
+    session({
+        secret:"kanban",
+        resave:false,
+        saveUninitialized:true,
+    })
+)
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
